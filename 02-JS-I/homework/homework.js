6de86bc964041200ca9fa3bb599cb52220dd12b3
gean <i202114701@cibertec.edu.pe>
2022-05-23 12:27:53 -0500

    hol

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "Juan Francisco";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 10;
+const nuevoNum = 20;
 
 // Crea una variable booleana:
 const nuevoBool = true;
